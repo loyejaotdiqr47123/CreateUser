213415a7cea64e799dc65851f7d075d4385319dd
bobo <102934154+loyejaotdiqr47123@users.noreply.github.com>
2023-08-17 19:20:50 +0800

    Update CreateUser.go

--- CreateUser.go
+++ CreateUser.go
@@ -8,31 +8,27 @@ import (
 )
 
 var (
-	netapi32DLL      = syscall.NewLazyDLL("netapi32.dll")
-	netUserAdd       = netapi32DLL.NewProc("NetUserAdd")
-	usrerrInfo       = netapi32DLL.NewProc("NetUserGetInfo")
-)
-
-const (
-	NERR_Success          = 0
-	ERROR_ACCESS_DENIED   = 5
-	ERROR_ALREADY_EXISTS  = 183
-	ERROR_INVALID_PARAMETER = 87
+	netapi32DLL           = syscall.NewLazyDLL("netapi32.dll")
+	netUserAddProc        = netapi32DLL.NewProc("NetUserAdd")
+	netLocalGroupAddProc  = netapi32DLL.NewProc("NetLocalGroupAddMembers")
+	userPrivUser          = 1
+	userFlagScript        = 1 << 0
+	userFlagPasswordCantChange = 1 << 6
+	userFlagDontExpirePasswd  = 1 << 7
 )
 
 type USER_INFO_1 struct {
-	usri1_name       *uint16
-	usri1_password   *uint16
-	usri1_password_age uint32
-	usri1_priv       uint32
-	usri1_home_dir   *uint16
-	usri1_comment    *uint16
-	usri1_flags      uint32
+	usri1_name      *uint16
+	usri1_password  *uint16
+	usri1_priv      uint32
+	usri1_home_dir  *uint16
+	usri1_comment   *uint16
+	usri1_flags     uint32
 	usri1_script_path *uint16
 }
 
-func NetUserAdd(servername *uint16, level uint32, buf *byte, parm_err *uint32) uint32 {
-	ret, _, _ := netUserAdd.Call(
+func NetUserAdd(servername *uint16, level uint32, buf *USER_INFO_1, parm_err *uint32) uint32 {
+	ret, _, _ := netUserAddProc.Call(
 		uintptr(unsafe.Pointer(servername)),
 		uintptr(level),
 		uintptr(unsafe.Pointer(buf)),
@@ -41,33 +37,44 @@ func NetUserAdd(servername *uint16, level uint32, buf *byte, parm_err *uint32) u
 	return uint32(ret)
 }
 
+func NetLocalGroupAddMembers(servername *uint16, groupname *uint16, level uint32, buf *USER_INFO_1, totalentries uint32) uint32 {
+	ret, _, _ := netLocalGroupAddProc.Call(
+		uintptr(unsafe.Pointer(servername)),
+		uintptr(unsafe.Pointer(groupname)),
+		uintptr(level),
+		uintptr(unsafe.Pointer(buf)),
+		uintptr(totalentries),
+	)
+	return uint32(ret)
+}
+
 func main() {
 	if len(os.Args) != 3 {
-		fmt.Println("用法: CreateUser.exe <username> <password>")
-		os.Exit(1)
+		fmt.Println("用法: CreateUser.exe username password")
+		return
 	}
 
 	username := syscall.StringToUTF16Ptr(os.Args[1])
 	password := syscall.StringToUTF16Ptr(os.Args[2])
 
-	var ui1 USER_INFO_1
-	ui1.usri1_name = username
-	ui1.usri1_password = password
-	ui1.usri1_priv = 1
-	ui1.usri1_flags = 0x200
-	ui1.usri1_home_dir = nil
-	ui1.usri1_comment = nil
+	var ui USER_INFO_1
+	ui.usri1_name = username
+	ui.usri1_password = password
+	ui.usri1_priv = userPrivUser
+	ui.usri1_flags = userFlagScript | userFlagPasswordCantChange | userFlagDontExpirePasswd
 
-	ret := NetUserAdd(nil, 1, (*byte)(unsafe.Pointer(&ui1)), nil)
-	if ret == NERR_Success {
-		fmt.Println("用户建新成功.")
-	} else if ret == ERROR_ACCESS_DENIED {
-		fmt.Println("拒绝访问，请以管理员权限执行")
-	} else if ret == ERROR_ALREADY_EXISTS {
-		fmt.Println("用户已存在.")
-	} else if ret == ERROR_INVALID_PARAMETER {
-		fmt.Println("无效参数.")
-	} else {
-		fmt.Printf("错误代码: %d\n", ret)
+	err := NetUserAdd(nil, 1, &ui, nil)
+	if err != 0 {
+		fmt.Printf("建新用户错误: %d\n", err)
+		return
 	}
+
+	groupname := syscall.StringToUTF16Ptr("Users")
+	err = NetLocalGroupAddMembers(nil, groupname, 3, &ui, 1)
+	if err != 0 {
+		fmt.Printf("添加用户组错误: %d\n", err)
+		return
+	}
+
+	fmt.Printf("添加用户和用户组成功", os.Args[1])
 }

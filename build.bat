219f65be8f17108824900a0e784960e8b88494ad
bobo <102934154+loyejaotdiqr47123@users.noreply.github.com>
2023-08-17 16:34:02 +0800

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 @echo off
-cd build && C:\Program Files (x86)\Windows Kits\10\bin\10.0.19041.0\x64\cl.exe /LD CreateUserDLL.cpp /link /OUT:CreateUserDLL.dll netapi32.lib
+cd build && "C:\Program Files (x86)\Windows Kits\10\bin\10.0.19041.0\x64\cl.exe" /LD CreateUserDLL.cpp /link /OUT:CreateUserDLL.dll netapi32.lib

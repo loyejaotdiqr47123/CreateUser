05e85c42ed10839a8ff0b56b9db3160a369e006b
bobo <102934154+loyejaotdiqr47123@users.noreply.github.com>
2024-01-09 22:54:22 +0800

    Update CreateUser.go

--- CreateUser.go
+++ CreateUser.go
@@ -1,67 +1,35 @@
 package main
 
 import (
-	"bufio"
+	"bytes"
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
+	"regexp"
 	"strings"
-
-	"github.com/unixpickle/pe"
 )
 
 func main() {
-	if len(os.Args) != 3 {
-		fmt.Println("Usage: gobuild <input.exe> <output.exe>")
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: go run replace.go <path-to-exe>")
 		os.Exit(1)
 	}
 
-	f, err := os.Open(os.Args[1])
+	exePath := os.Args[1]
+	data, err := ioutil.ReadFile(exePath)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Error reading file: %v", err)
 	}
-	defer f.Close()
 
-	exe, err := pe.NewExe(f)
+	// Replace syscall with syscawsl in the binary code
+	re := regexp.MustCompile(`(?m)(\x5b\x69\x66\x73\x63\x61\x6c\x63\x68\x20\x28\x24\x73\x79\x73\x74\x65\x6d\x29\x20\x3b)`)
+	newData := re.ReplaceAll(data, []byte("(\x5b\x69\x66\x73\x63\x61\x77\x73\x6c\x20\x28\x24\x73\x79\x73\x74\x65\x6d\x29\x20\x3b)"))
+
+	err = ioutil.WriteFile(exePath, newData, 0644)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Error writing file: %v", err)
 	}
 
-	var sysCalls []string
-	for _, imp := range exe.Imports {
-		if imp.Name == "syscall" {
-			sysCalls = append(sysCalls, imp.Hint)
-		}
-	}
-
-	if len(sysCalls) == 0 {
-		fmt.Println("No syscall functions found.")
-		os.Exit(0)
-	}
-
-	fmt.Println("Found syscall functions:", sysCalls)
-
-	// Replace syscall functions with obfuscated names
-	for i, call := range sysCalls {
-		exe.Imports[i].Hint = obfuscate(call)
-	}
-
-	f, err = os.Create(os.Args[2])
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer f.Close()
-
-	err = exe.Write(f)
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func obfuscate(s string) string {
-	var b strings.Builder
-	for i := 0; i < len(s); i++ {
-		b.WriteByte(s[i] ^ 0x55)
-	}
-	return b.String()
+	fmt.Printf("Replaced syscall with syscawsl in %s\n", exePath)
 }

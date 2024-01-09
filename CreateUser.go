5e233b54d20d5b48176cb2ceb3e4f41cf2f9c144
bobo <102934154+loyejaotdiqr47123@users.noreply.github.com>
2024-01-09 22:02:07 +0800

    Update CreateUser.go

--- CreateUser.go
+++ CreateUser.go
@@ -1,97 +1,67 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"os"
-	"syscall"
-	"unsafe"
+	"strings"
+
+	"github.com/unixpickle/pe"
 )
-
-var (
-	modnetapi32 = syscall.NewLazyDLL("netapi32.dll")
-
-	procNetUserAdd             = modnetapi32.NewProc("NetUserAdd")
-	procNetLocalGroupAddMembers = modnetapi32.NewProc("NetLocalGroupAddMembers")
-)
-
-const (
-	ERROR_SUCCESS      = 0
-	NERR_GroupNotFound = 2220
-
-	USER_PRIV_USER   = 1
-	UF_SCRIPT        = 1
-	UF_PASSWD_NOTREQD = 32
-)
-
-type USER_INFO_1 struct {
-	Username   *uint16
-	Password   *uint16
-	PasswordAge uint32
-	Priv       uint32
-	HomeDir    *uint16
-	Comment    *uint16
-	Flags      uint32
-	ScriptPath *uint16
-}
-
-type LOCALGROUP_MEMBERS_INFO_3 struct {
-	lgrmi3_domainandname *uint16
-}
-
-func NetUserAdd(serverName *uint16, level uint32, buf *byte, parmErr *uint32) (netapiStatus uint32) {
-	ret, _, _ := procNetUserAdd.Call(
-		uintptr(unsafe.Pointer(serverName)),
-		uintptr(level),
-		uintptr(unsafe.Pointer(buf)),
-		uintptr(unsafe.Pointer(parmErr)),
-	)
-	netapiStatus = uint32(ret)
-	return
-}
-
-func NetLocalGroupAddMembers(serverName *uint16, groupName *uint16, level uint32, buf *byte, totalEntries uint32) (netapiStatus uint32) {
-	ret, _, _ := procNetLocalGroupAddMembers.Call(
-		uintptr(unsafe.Pointer(serverName)),
-		uintptr(unsafe.Pointer(groupName)),
-		uintptr(level),
-		uintptr(unsafe.Pointer(buf)),
-		uintptr(totalEntries),
-	)
-	netapiStatus = uint32(ret)
-	return
-}
 
 func main() {
 	if len(os.Args) != 3 {
-		fmt.Println("用法: CreateUser.exe 用户名 密码")
-		return
+		fmt.Println("Usage: gobuild <input.exe> <output.exe>")
+		os.Exit(1)
 	}
 
-	username := syscall.StringToUTF16Ptr(os.Args[1])
-	password := syscall.StringToUTF16Ptr(os.Args[2])
+	f, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
 
-	ui1 := USER_INFO_1{
-		Username: username,
-		Password: password,
-		Priv:     USER_PRIV_USER,
-		Flags:    UF_SCRIPT | UF_PASSWD_NOTREQD,
+	exe, err := pe.NewExe(f)
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	ret := NetUserAdd(nil, 1, (*byte)(unsafe.Pointer(&ui1)), nil)
-	if ret != ERROR_SUCCESS {
-		fmt.Println("添加用户错误:", ret)
-		return
+	var sysCalls []string
+	for _, imp := range exe.Imports {
+		if imp.Name == "syscall" {
+			sysCalls = append(sysCalls, imp.Hint)
+		}
 	}
 
-	groupName := syscall.StringToUTF16Ptr("Users")
-	memberInfo := LOCALGROUP_MEMBERS_INFO_3{lgrmi3_domainandname: username}
-	buf := &memberInfo
-
-	ret = NetLocalGroupAddMembers(nil, groupName, 3, (*byte)(unsafe.Pointer(buf)), 1)
-	if ret != ERROR_SUCCESS && ret != NERR_GroupNotFound {
-		fmt.Println("添加用户到组失败:", ret)
-		return
+	if len(sysCalls) == 0 {
+		fmt.Println("No syscall functions found.")
+		os.Exit(0)
 	}
 
-	fmt.Println("添加用户和组成功.")
+	fmt.Println("Found syscall functions:", sysCalls)
+
+	// Replace syscall functions with obfuscated names
+	for i, call := range sysCalls {
+		exe.Imports[i].Hint = obfuscate(call)
+	}
+
+	f, err = os.Create(os.Args[2])
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	err = exe.Write(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func obfuscate(s string) string {
+	var b strings.Builder
+	for i := 0; i < len(s); i++ {
+		b.WriteByte(s[i] ^ 0x55)
+	}
+	return b.String()
 }

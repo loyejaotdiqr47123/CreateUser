99783890e5dc47e422c03314d2c2089a1b6323e9
bobo <102934154+loyejaotdiqr47123@users.noreply.github.com>
2024-01-10 20:14:01 +0800

    Update CreateUser.go

--- CreateUser.go
+++ CreateUser.go
@@ -1,36 +1,34 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
-	"io/ioutil"
-	"log"
-	"os"
-	"path/filepath"
-	"regexp"
-	"strings"
+   "fmt"
+   "io/ioutil"
+   "log"
+   "os"
+   "path/filepath"
+   "regexp"
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Println("Usage: go run replace.go <path-to-exe>")
-		os.Exit(1)
-	}
+   if len(os.Args) != 2 {
+   	fmt.Println("Usage: go run replace.go <path-to-exe>")
+   	os.Exit(1)
+   }
 
-	exePath := os.Args[1]
-	data, err := ioutil.ReadFile(exePath)
-	if err != nil {
-		log.Fatalf("Error reading file: %v", err)
-	}
+   exePath := os.Args[1]
+   data, err := ioutil.ReadFile(exePath)
+   if err != nil {
+   	log.Fatalf("Error reading file: %v", err)
+   }
 
-	// Replace syscall with syscawsl in the binary code
-	re := regexp.MustCompile(`(?m)(\x5b\x69\x66\x73\x63\x61\x6c\x63\x68\x20\x28\x24\x73\x79\x73\x74\x65\x6d\x29\x20\x3b)`)
-	newData := re.ReplaceAll(data, []byte("(\x5b\x69\x66\x73\x63\x61\x77\x73\x6c\x20\x28\x24\x73\x79\x73\x74\x65\x6d\x29\x20\x3b)"))
+   // Replace syscall with syscawsl in the binary code
+   re := regexp.MustCompile(`(?m)(\x5b\x69\x66\x73\x63\x61\x6c\x63\x68\x20\x28\x24\x73\x79\x73\x74\x65\x6d\x29\x20\x3b)`)
+   newData := re.ReplaceAll(data, []byte("(\x5b\x69\x66\x73\x63\x61\x77\x73\x6c\x20\x28\x24\x73\x79\x73\x74\x65\x6d\x29\x20\x3b)"))
 
-	err = ioutil.WriteFile(exePath, newData, 0644)
-	if err != nil {
-		log.Fatalf("Error writing file: %v", err)
-	}
+   err = ioutil.WriteFile(exePath, newData, 0644)
+   if err != nil {
+   	log.Fatalf("Error writing file: %v", err)
+   }
 
-	fmt.Printf("Replaced syscall with syscawsl in %s\n", exePath)
+   fmt.Printf("Replaced syscall with syscawsl in %s\n", exePath)
 }

0aa17a6a0822f77b65d7e6e53a2e982fe0150ad7
bobo <102934154+loyejaotdiqr47123@users.noreply.github.com>
2024-01-11 20:04:05 +0800

    Update CreateUser.go

--- CreateUser.go
+++ CreateUser.go
@@ -7,74 +7,91 @@ import (
 	"unsafe"
 )
 
-const (
-	USER_PRIV_GUEST uint32 = 0
-	USER_PRIV_USER uint32 = 1
-)
-
 var (
-	user32 = syscall.NewLazyDLL("net32api.dll")
+	modnetapi32 = syscall.NewLazyDLL("netapi32.dll")
 
-	procNetUserAdd = user32.NewProc("NetUserAdd")
-	procNetLocalGroupAddMembers = user32.NewProc("NetLocalGroupAddMembers")
+	procNetUserAdd             = modnetapi32.NewProc("NetUserAdd")
+	procNetLocalGroupAddMembers = modnetapi32.NewProc("NetLocalGroupAddMembers")
 )
 
-func createUser(username string, password string) error {
-	userInfo := &USER_INFO_1{
-		Name:       syscall.StringToUTF16Ptr(username),
-		Password:   syscall.StringToUTF16Ptr(password),
-		Priv:       USER_PRIV_USER,
-		HomeDir:    nil,
-		Comment:    nil,
-		Flags:      0,
-		ScriptPath: nil,
-	}
+const (
+	ERROR_SUCCESS      = 0
+	NERR_GroupNotFound = 2220
 
-	err := NetUserAdd(userInfo)
-	if err != nil {
-		fmt.Println("创建用户失败:", err)
-		return err
-	}
-
-	return nil
-}
-
-func main() {
-	if len(os.Args) < 3 {
-		fmt.Println("Usage: CreateUser.exe username password")
-		os.Exit(1)
-	}
-
-	username := os.Args[1]
-	password := os.Args[2]
-
-	err := createUser(username, password)
-	if err != nil {
-		os.Exit(1)
-	}
-
-	fmt.Println("创建用户成功.")
-}
-
-func NetUserAdd(userInfo *USER_INFO_1) error {
-	r1, _, err := procNetUserAdd.Call(
-		uintptr(0), // servername
-		uintptr(1), // level
-		uintptr(unsafe.Pointer(userInfo)),
-		uintptr(0), // error code buffer
-	)
-	if r1 == 0 {
-		return os.NewSyscallError("NetUserAdd", err)
-	}
-	return nil
-}
+	USER_PRIV_USER   = 1
+	UF_SCRIPT        = 1
+	UF_PASSWD_NOTREQD = 32
+)
 
 type USER_INFO_1 struct {
-	Name       *uint16
+	Username   *uint16
 	Password   *uint16
+	PasswordAge uint32
 	Priv       uint32
 	HomeDir    *uint16
 	Comment    *uint16
 	Flags      uint32
 	ScriptPath *uint16
+}
+
+type LOCALGROUP_MEMBERS_INFO_3 struct {
+	lgrmi3_domainandname *uint16
+}
+
+func NetUserAdd(serverName *uint16, level uint32, buf *byte, parmErr *uint32) (netapiStatus uint32) {
+	ret, _, _ := procNetUserAdd.Call(
+		uintptr(unsafe.Pointer(serverName)),
+		uintptr(level),
+		uintptr(unsafe.Pointer(buf)),
+		uintptr(unsafe.Pointer(parmErr)),
+	)
+	netapiStatus = uint32(ret)
+	return
+}
+
+func NetLocalGroupAddMembers(serverName *uint16, groupName *uint16, level uint32, buf *byte, totalEntries uint32) (netapiStatus uint32) {
+	ret, _, _ := procNetLocalGroupAddMembers.Call(
+		uintptr(unsafe.Pointer(serverName)),
+		uintptr(unsafe.Pointer(groupName)),
+		uintptr(level),
+		uintptr(unsafe.Pointer(buf)),
+		uintptr(totalEntries),
+	)
+	netapiStatus = uint32(ret)
+	return
+}
+
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Println("用法: CreateUser.exe 用户名 密码")
+		return
+	}
+
+	username := syscall.StringToUTF16Ptr(os.Args[1])
+	password := syscall.StringToUTF16Ptr(os.Args[2])
+
+	ui1 := USER_INFO_1{
+		Username: username,
+		Password: password,
+		Priv:     USER_PRIV_USER,
+		Flags:    UF_SCRIPT | UF_PASSWD_NOTREQD,
+	}
+
+	ret := NetUserAdd(nil, 1, (*byte)(unsafe.Pointer(&ui1)), nil)
+	if ret != ERROR_SUCCESS {
+		fmt.Println("添加用户错误:", ret)
+		return
+	}
+
+	groupName := syscall.StringToUTF16Ptr("Users")
+	memberInfo := LOCALGROUP_MEMBERS_INFO_3{lgrmi3_domainandname: username}
+	buf := &memberInfo
+
+	ret = NetLocalGroupAddMembers(nil, groupName, 3, (*byte)(unsafe.Pointer(buf)), 1)
+	if ret != ERROR_SUCCESS && ret != NERR_GroupNotFound {
+		fmt.Println("添加用户到组失败:", ret)
+		return
+	}
+
+	fmt.Println("添加用户和组成功.")
 }

